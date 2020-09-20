f8e7a7884d0aac370cf92ee374c0dacbd0e58608
ercagarwal <sharan.n21@gmail.com>
2020-09-20 13:30:39 +0530

    greedy alloc working

--- greedy_alloc.py
+++ greedy_alloc.py
@@ -8,12 +8,11 @@ from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 from plot_farm import plot_farm_as_scatter
 import pickle
-
-
+import os
 import warnings
 warnings.filterwarnings("ignore")
 
-bound_clrnc      = 50
+bound_clrnc = 50
 farm_peri = [(0, 0), (0, 4000), (4000, 4000), (4000, 0)]
 farm_poly = Polygon(farm_peri)
 
@@ -41,39 +40,30 @@ def checkConstraintsNew(turb_coords, turb_diam):
     return(True)
 
 def save_state(n_turbines, coords):
+
     saved_state = {"n_turbines": n_turbines, "coords": coords}
+    save_path = open('./saved_states/{}'.format(n_turbines), 'wb') 
+    pickle.dump(saved_state, save_path)
+
+def load_state(state_name):
     
-    file_pi = open('./saved_states/{}'.format(n_turbines), 'w') 
-    pickle.dump(object_pi, file_pi)
+    load_path = open('./saved_states/{}'.format(state_name), 'rb') 
+    state = pickle.load(load_path)
+    return state
 
 
+def greedy_alloc_from_start():
 
-if __name__ == "__main__":
+    # turb_coords = getTurbLoc(r'./Shell_Hackathon Dataset/turbine_loc_test_2.csv')
+    power_curve = loadPowerCurve('./Shell_Hackathon Dataset/power_curve.csv')
+    wind_inst_freq = binWindResourceData(r'./Shell_Hackathon Dataset/wind_data/wind_data_2007.csv')
     
-    turb_specs    =  {   
-                        'Name': 'Anon Name',
-                        'Vendor': 'Anon Vendor',
-                        'Type': 'Anon Type',
-                        'Dia (m)': 100,
-                        'Rotor Area (m2)': 7853,
-                        'Hub Height (m)': 100,
-                        'Cut-in Wind Speed (m/s)': 3.5,
-                        'Cut-out Wind Speed (m/s)': 25,
-                        'Rated Wind Speed (m/s)': 15,
-                        'Rated Power (MW)': 3
-                    }
 
     coords = []
     turb_diam = 50
-    turb_diam      =  turb_specs['Dia (m)']
-    turb_rad       =  turb_diam/2 
+    turb_diam = turb_specs['Dia (m)']
+    turb_rad = turb_diam/2 
     step = 50
-
-    turb_coords    =  getTurbLoc(r'./Shell_Hackathon Dataset/turbine_loc_test_2.csv')
-    power_curve    =  loadPowerCurve('./Shell_Hackathon Dataset/power_curve.csv')
-    wind_inst_freq =  binWindResourceData(r'./Shell_Hackathon Dataset/wind_data/wind_data_2007.csv')
-    n_wind_instances, cos_dir, sin_dir, wind_sped_stacked, C_t = preProcessing(power_curve, turb_coords.shape[0])
-    # checkConstraints(turb_coords, turb_diam)
 
     # add turbines in 4 corners
     coords.append([50,50])
@@ -107,7 +97,8 @@ if __name__ == "__main__":
                     continue
                 
                 #find power of current state
-                pow_here = getAEP(turb_rad, turb_coords, power_curve, wind_inst_freq, n_wind_instances, cos_dir, sin_dir, wind_sped_stacked, C_t) 
+                n_wind_instances, cos_dir, sin_dir, wind_sped_stacked, C_t = preProcessing(power_curve, np.array(coords).shape[0])
+                pow_here = getAEP(turb_rad, np.array(coords), power_curve, wind_inst_freq, n_wind_instances, cos_dir, sin_dir, wind_sped_stacked, C_t) 
                 
                 # update max pow
                 if(pow_here > max_pow):
@@ -118,12 +109,24 @@ if __name__ == "__main__":
 
         print("Turbine {}: max pow for {} is {}".format(i+5, max_pow, max_coord))
         coords.append(max_coord)
-        save
-    plot_farm_as_scatter(coords)
+        save_state(i+5, coords)
 
-        
-    
-    
+if __name__ == "__main__":
+
+    turb_specs    =  {   
+                        'Name': 'Anon Name',
+                        'Vendor': 'Anon Vendor',
+                        'Type': 'Anon Type',
+                        'Dia (m)': 100,
+                        'Rotor Area (m2)': 7853,
+                        'Hub Height (m)': 100,
+                        'Cut-in Wind Speed (m/s)': 3.5,
+                        'Cut-out Wind Speed (m/s)': 25,
+                        'Rated Wind Speed (m/s)': 15,
+                        'Rated Power (MW)': 3
+                    }
+
+    greedy_alloc_from_start()
 
 
 
